d9be70464ee2714376f865869ab94e68b8c495ce
mtt0212 <thanhmai.cts@gmai.com>
2015-12-11 08:42:10 +0700

    nhầm lẫn

--- version 4/4.1/timkiem.php
+++
@@ -1,7 +0,0 @@
-
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<form action="xulytk.php" method="post">
-        <input type="text" name="timkiem" value="">
-        <input type="submit" value="Tim">
-</form>
--- version 4/4.2/mod.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mod = isset($_GET["mod"])?$_GET["mod"]:"";
-if($mod == "delete")
-{
-	include "delete.php";
-}
-
-
-?>
